d8ad5c12242c911e87cfb03566fc9eb635b5142d
craftfetig <craftfetuuuz21@gmail.com>
2021-08-24 22:51:33 -0400

    java project eclipse

---
+++ helloWorld/src/com/imac/gitdemo.java
@@ -0,0 +1,18 @@
+package com.imac;
+
+public class gitdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello java world");
+		System.out.println("welcome kashe");
+		System.out.println("welcome new");
+		System.out.println("welcome new two");
+		
+		
+		
+		
+
+	}
+
+}
